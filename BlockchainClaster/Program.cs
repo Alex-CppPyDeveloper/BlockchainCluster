423a7be0554ae4892018cc2a8fe09267f1c6f4c4
Alex <alexej.eroshin@yandex.ru>
2020-10-03 15:22:03 +0300

    version 1.0

---
+++ BlockchainClaster/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using BlockChainNode;
+
+using System.Net.Http;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Akka.Dispatch;
+using System.Text;
+
+namespace BlockchainClaster
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+
+            string url = "chains/main/blocks/head/header";
+
+            List<string> urls = new List<string>() { "https://rpc.tzkt.io/mainnet/", "https://mainnet-tezos.giganode.io/", "https://mainnet.smartpy.io/" };
+
+            string i = null;
+
+            while (Convert.ToInt16(i) != 2 )
+            {
+                Console.WriteLine("Добавить url?");
+                i = Console.ReadLine();
+
+                if (Convert.ToInt16(i) != 2)
+                {
+                    string new_base_url = Console.ReadLine();
+                    urls.Add(new_base_url);
+                }
+                else break;
+
+            }
+
+            var nodeCluster = new NodeCluster(urls);
+           
+            Head LatestHead = await nodeCluster.GetLatestHead(url);
+
+            Console.WriteLine($"Head.Url: {LatestHead.Url}");
+            Console.WriteLine($"Head.ChainId: {LatestHead.Chain_id}");
+            Console.WriteLine($"Head.Hash: {LatestHead.Hash}");
+            Console.WriteLine($"Head.Predecessor: {LatestHead.Predecessor}");
+            Console.WriteLine($"Head.Level: {LatestHead.Level}");
+            Console.WriteLine($"Head.Timestamp: {LatestHead.Timestamp}");
+        }
+
+    }
+
+}
+    
+
---
+++ BlockchainClaster/СlassBlockchainNode.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+using System.Net.Http;
+using System.Net;
+using System.IO;
+
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BlockChainNode
+{
+    class  Head
+    {
+        public string Url { get; set; }
+        public string Chain_id { get; set; }
+        public string Hash { get; set; }
+        public string Predecessor { get; set; }
+        public int Level { get; set; }
+        public DateTime Timestamp { get; set; }
+
+
+    }
+    class СlassBlockchainNode
+    {
+        public string base_url;
+
+      public СlassBlockchainNode(string base_url)
+        {
+
+            this.base_url = base_url ;
+
+        }
+
+        public async Task<Head> GetHead(string url)
+        
+        {
+      
+            try
+            {
+          
+                Console.WriteLine("Hello World!");
+                HttpClient client = new HttpClient();
+                
+                 string s = await client.GetStringAsync(url);
+                
+                Head parsed = JsonConvert.DeserializeObject<Head>(s);
+
+                Console.WriteLine($"\n {url}");
+                Console.WriteLine($"Head.ChainId: {parsed.Chain_id}");
+                Console.WriteLine($"Head.Hash: {parsed.Hash}");
+                Console.WriteLine($"Head.Predecessor: {parsed.Predecessor}");
+                Console.WriteLine($"Head.Level: {parsed.Level}");
+                Console.WriteLine($"Head.Timestamp: {parsed.Timestamp}");
+
+                parsed.Url = url;
+                return parsed;
+
+            }
+
+            catch (HttpRequestException e)
+            {
+                Head parsed= null;
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return parsed;
+            }
+
+            
+        }
+      
+    }
+
+    class NodeCluster
+    {
+        public List<string> urls;
+
+        public NodeCluster(List<string> urls)
+        {
+
+            this.urls = urls;
+
+        }
+
+        public async Task<Head> GetLatestHead(string url)
+        {
+          
+            var check_Timestamp = new DateTime(2017, 12, 12, 12, 12, 12);
+
+            var heads = new List<Head>();
+
+            foreach (string item in urls)
+            {
+                
+                СlassBlockchainNode classBlockchain = new СlassBlockchainNode(item);
+                heads.Add(await classBlockchain.GetHead(item + url));
+
+                Console.WriteLine("Hello");
+            }
+
+           var latest_node = new Head();
+
+            foreach (Head item in heads)
+            {
+
+
+                if (item.Timestamp > check_Timestamp)
+                {
+
+                    latest_node = item;
+                    check_Timestamp = latest_node.Timestamp;
+
+                }
+               
+            }
+
+            return latest_node;
+        }
+    }
+}
